102f13e08976e67ad6e3a4e50b92f578d56ff374
Harrison McCarty <hmccarty@pm.me>
2024-10-26 15:59:11 -0700

    fixed tmux shell

--- home-manager/tmux.nix
+++ home-manager/tmux.nix
@@ -7,14 +7,18 @@
     historyLimit = 100000;
     keyMode = "vi";
     extraConfig = ''
+      set -g default-command ""
       set-window-option -g mode-keys vi
       bind h select-pane -L
       bind j select-pane -D
       bind k select-pane -U
       bind l select-pane -R
+      bind p command-prompt -p 'Save history to:' -I '~/tmux.history' \
+        'capture-pane -S -32768 ; save-buffer %1 ; delete-buffer'
       bind-key -T copy-mode-vi v send -X begin-selection
       bind-key -T copy-mode-vi V send -X select-line
-      bind-key -T copy-mode-vi y send -X copy-pipe-and-cancel
+      bind-key -T copy-mode-vi y send -X copy-pipe-and-cancel \
+        'xclip -in -selection clipboard'
     '';
   };
 }
